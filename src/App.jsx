8183adde67592132974368a9eede36b85e9010e3
Charles Odia <charlesodia48@gmail.com>
2025-05-15 22:56:54 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Hero from './sections/Hero.jsx'
+import ShowcaseSection from "./sections/ShowcaseSection.jsx";
+import NavBar from "./components/NavBar.jsx";
+import FeatureCards from "./sections/FeatureCards.jsx";
+
+const App = () => {
+    return (
+  <>
+      <NavBar />
+      <Hero />
+      <ShowcaseSection />
+      <FeatureCards />
+  </>
+    )
+}
+export default App
